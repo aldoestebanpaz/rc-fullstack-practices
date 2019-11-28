c2659b83bb90cd1467f89403427eeb14e76dc2dc
Aldo Paz <estebanpazutn@gmail.com>
2019-11-28 08:46:33 -0300

    add Header and MemeGenerator components

---
+++ meme-generator/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="any problem?" />
+        <p>Meme Generator</p>
+      </header>
+    );
+  }
+}
